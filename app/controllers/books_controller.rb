343e3b0557b1b89cec18add993986cdf3f870a59
tyauszuu <124164778+tyauszuu@users.noreply.github.com>
2023-02-21 22:17:33 +0000

    [Add]削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,26 @@ class BooksController < ApplicationController
   def create
     @book=Book.new(book_params)
     @book.save
+    redirect_to book_path(@book)
+  end
+  
+  def show
+    @book=Book.find(params[:id])
+  end
+  
+  def edit
+    @book=Book.find(params[:id])
+  end
+  
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
     redirect_to '/books'
   end
   
@@ -14,4 +34,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title,:body)
   end
+
 end
