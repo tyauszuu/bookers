d427375d5247b93b9dd80e65a363e771cfa235b8
tyauszuu <124164778+tyauszuu@users.noreply.github.com>
2023-02-21 02:02:44 +0000

    [Add]homesコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'homes/top'
   get 'books'=>'books#index'
   post 'books'=>'books#create'
+  get 'books/show'
+  get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
