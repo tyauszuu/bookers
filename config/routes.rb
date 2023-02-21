b2183d28830d4ed47c455a984be33f8ce50a4136
tyauszuu <124164778+tyauszuu@users.noreply.github.com>
2023-02-21 00:23:20 +0000

    [Add]投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
+  get 'books'=>'books#index'
+  post 'books'=>'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
